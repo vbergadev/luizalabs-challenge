9c23841d73db1360d0d3fd4d3c10c52c983b51e1
Victor Pina Bergamini <victorpinabergamini@Victors-MacBook-Pro.local>
2024-05-26 14:15:25 -0300

    first commit

---
+++ src/main/java/br/com/luizalabs_challenge/luizalabs/LuizalabsApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs_challenge.luizalabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizalabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizalabs_challenge/luizalabs/LuizalabsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs_challenge.luizalabs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizalabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
