1d62ee481be7fc478e8f3f4912b0b7701d09c693
Victor Pina Bergamini <victorpinabergamini@Victors-MacBook-Pro.local>
2024-05-27 21:53:41 -0300

    feat: create repository

---
+++ src/main/java/br/com/luizalabs_challenge/luizalabs/repositories/ScheduleCommunicationRepository.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs_challenge.luizalabs.repositories;
+
+import br.com.luizalabs_challenge.luizalabs.models.ScheduleCommunication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ScheduleCommunicationRepository extends JpaRepository<ScheduleCommunication, UUID>{
+    List<ScheduleCommunication> findByActive(boolean isActive);
+}
